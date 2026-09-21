7dd38a3f255416613067cd55e918af6303ac44b0
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-09-22 06:44:40 +0700

    Menambahkan tabel pembayaran untuk pembelian dan penjualan

---
+++ database/migrations/2026_09_21_000010_create_pembayaran_pembelian_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('pembayaran_pembelian', function (Blueprint $table) {
+            $table->id();
+            $table->char('kode', 15)->unique();
+            $table->date('tanggal');
+            $table->unsignedInteger('jumlah');
+            $table->string('metode_pembayaran');
+            $table->timestamps();
+
+            $table->foreignId('pembelian_id')->constrained('pembelian');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayaran_pembelian');
+    }
+};
