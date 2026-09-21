3663371c4a9c0fe6861f62396613348cb9ae6096
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-09-21 23:58:11 +0700

    Membuat seeder/data dummy pelanggan

--- database/seeders/PelangganSeeder.php
+++ database/seeders/PelangganSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -11,10 +10,166 @@ class PelangganSeeder extends Seeder
     public function run(): void
     {
         DB::table('pelanggan')->insert([
-            'kode'          => 'PLG-202609-0001',
-            'nama'          => 'Yani Tristanti',
-            'nomor_telepon' => '0881010788808',
-            'alamat'        => 'Jl. Graha Angkasa RRI Surabaya No.A15, Balongpoh, Kedungrejo, Kec. Waru, Kabupaten Sidoarjo, Jawa Timur',
+            [
+                'kode'          => 'PLG-202609-0001',
+                'nama'          => 'Andi Pratama',
+                'nomor_telepon' => '081234567801',
+                'alamat'        => 'Jl. Melati No. 12, Surabaya',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0002',
+                'nama'          => 'Siti Rahmawati',
+                'nomor_telepon' => '082145678902',
+                'alamat'        => 'Jl. Mawar No. 8, Malang',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0003',
+                'nama'          => 'Budi Santoso',
+                'nomor_telepon' => '083156789013',
+                'alamat'        => 'Jl. Kenanga No. 25, Sidoarjo',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0004',
+                'nama'          => 'Dewi Lestari',
+                'nomor_telepon' => '085267890124',
+                'alamat'        => 'Jl. Anggrek No. 17, Gresik',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0005',
+                'nama'          => 'Rizky Maulana',
+                'nomor_telepon' => '081378901235',
+                'alamat'        => 'Jl. Dahlia No. 6, Surabaya',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0006',
+                'nama'          => 'Nur Aisyah',
+                'nomor_telepon' => '082289012346',
+                'alamat'        => 'Jl. Flamboyan No. 31, Mojokerto',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0007',
+                'nama'          => 'Fajar Nugroho',
+                'nomor_telepon' => '083390123457',
+                'alamat'        => 'Jl. Cempaka No. 14, Kediri',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0008',
+                'nama'          => 'Putri Amelia',
+                'nomor_telepon' => '085401234568',
+                'alamat'        => 'Jl. Kamboja No. 9, Pasuruan',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0009',
+                'nama'          => 'Dimas Saputra',
+                'nomor_telepon' => '081512345679',
+                'alamat'        => 'Jl. Teratai No. 22, Surabaya',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0010',
+                'nama'          => 'Rina Wulandari',
+                'nomor_telepon' => '082623456780',
+                'alamat'        => 'Jl. Bougenville No. 18, Lamongan',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0011',
+                'nama'          => 'Yoga Firmansyah',
+                'nomor_telepon' => '083734567891',
+                'alamat'        => 'Jl. Sakura No. 5, Jombang',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0012',
+                'nama'          => 'Intan Permata',
+                'nomor_telepon' => '085845678912',
+                'alamat'        => 'Jl. Wijaya Kusuma No. 27, Surabaya',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0013',
+                'nama'          => 'Arif Hidayat',
+                'nomor_telepon' => '081956789023',
+                'alamat'        => 'Jl. Seroja No. 11, Banyuwangi',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0014',
+                'nama'          => 'Lina Marlina',
+                'nomor_telepon' => '082067890134',
+                'alamat'        => 'Jl. Tulip No. 16, Probolinggo',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0015',
+                'nama'          => 'Wahyu Setiawan',
+                'nomor_telepon' => '083178901245',
+                'alamat'        => 'Jl. Kemuning No. 3, Surabaya',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0016',
+                'nama'          => 'Maya Sari',
+                'nomor_telepon' => '085289012356',
+                'alamat'        => 'Jl. Lavender No. 20, Madiun',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0017',
+                'nama'          => 'Reza Kurniawan',
+                'nomor_telepon' => '081390123467',
+                'alamat'        => 'Jl. Puspa No. 7, Sidoarjo',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0018',
+                'nama'          => 'Nabila Putri',
+                'nomor_telepon' => '082401234578',
+                'alamat'        => 'Jl. Asoka No. 13, Malang',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0019',
+                'nama'          => 'Galih Ramadhan',
+                'nomor_telepon' => '083512345689',
+                'alamat'        => 'Jl. Kasturi No. 29, Surabaya',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
+            [
+                'kode'          => 'PLG-202609-0020',
+                'nama'          => 'Tia Anggraini',
+                'nomor_telepon' => '085623456790',
+                'alamat'        => 'Jl. Melur No. 10, Gresik',
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ],
         ]);
     }
 }
